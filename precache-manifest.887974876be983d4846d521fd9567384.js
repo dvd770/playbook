80f8afe2a2de3c6903c73992965ef3b6f859b119
Shon-Elias <shon.elias@gmail.com>
2020-09-06 13:59:05 +0300

    Updates

---
+++ precache-manifest.887974876be983d4846d521fd9567384.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ddd82c44664389cfba8365c5ce7aed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "365019ce66cf1a8dae68",
+    "url": "/static/css/2.71c10783.chunk.css"
+  },
+  {
+    "revision": "f628410bbfbd6d6acccb",
+    "url": "/static/css/main.0493f52e.chunk.css"
+  },
+  {
+    "revision": "365019ce66cf1a8dae68",
+    "url": "/static/js/2.84344b00.chunk.js"
+  },
+  {
+    "revision": "a2252ab558c25643e808ebcb56834eb1",
+    "url": "/static/js/2.84344b00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f628410bbfbd6d6acccb",
+    "url": "/static/js/main.e74af50f.chunk.js"
+  },
+  {
+    "revision": "3c27701234b2cee086ce",
+    "url": "/static/js/runtime-main.ee534919.js"
+  }
+]);
